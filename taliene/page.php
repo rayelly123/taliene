fc26f5699afbc5c401c217c860cde4e02fec367b
rayelly123 <rayelly.nunes@escola.pr.gov.br>
2025-09-22 11:29:54 -0300

    Add files via upload

---
+++ taliene/page.php
@@ -0,0 +1,16 @@
+<?php
+include "conexao.php"; // inclui o arquivo de conexão
+
+$result = mysqli_query($conexao, "SELECT * FROM user"); // exemplo de consulta
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1></h1>
+</body>
+</html>
