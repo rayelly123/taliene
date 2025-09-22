8d9b22293306332404ee4028c3aa620d2d7208bb
rayelly123 <rayelly.nunes@escola.pr.gov.br>
2025-09-22 11:37:33 -0300

    Add files via upload

--- taliene/page.php
+++ taliene/page.php
@@ -5,12 +5,43 @@ $result = mysqli_query($conexao, "SELECT * FROM user"); // exemplo de consulta
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <title>tec</title>
 </head>
 <body>
-<h1></h1>
+
+    <form>
+    <H3>cadastro</H3>
+    <input type="text" placeholder="Login" id="login">
+    <input type="password" placeholder="Senha" id="senha">
+    <input type="text" placeholder="telefone" id="telefone">
+    <input type="email" placeholder="email" id="email">
+    <input type="submit" onclick="logar(); return false">
+    </form>
+
+    <script>
+        function logar(){
+
+            var login = document.getElementById('login').value;
+            var senha = document.getElementById('senha').value;
+            
+            if(login == "admin" && senha == "admin"){
+                alert('Sucesso');
+                location.href = "home.html";
+            }else{
+                alert('usuario ou senha incorretos');
+            }
+
+
+        }
+
+    </script>
+       
 </body>
 </html>
+
+    
